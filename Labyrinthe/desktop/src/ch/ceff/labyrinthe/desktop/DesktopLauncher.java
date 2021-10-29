1844caab24f8edcc3cb431741f032ca3f37f8d49
Jack <jack.dobbs@ceff.ch>
2021-10-29 13:13:33 +0200

    Add all files

---
+++ Labyrinthe/desktop/src/ch/ceff/labyrinthe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ch.ceff.labyrinthe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ch.ceff.labyrinthe.Labyrinthe;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Labyrinthe(), config);
+	}
+}
